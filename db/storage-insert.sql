8e383b0ade03bf91738e4dd1bffd70dbc4fe1a19
MintMissy <dawidkostka480@gmail.com>
2021-12-06 18:08:27 +0100

    Create storage sql insert

---
+++ db/storage-insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+  `storage`(
+    `item_name`,
+    `item_quantity`,
+    `quantity_unit`,
+    `recommended_quantity`
+  )
+VALUES
+  ('Red Wine', 31, 'l', 12),
+  ('White Wine', 25, 'l', 15),
+  ('Milk', 40, 'l', 20),
+  ('Rice', 23, 'kg', 10),
+  ('Chicken Meat', 13, 'kg', 14),
+  ('Cream', 20, 'l', 8),
+  ('Honey', 5, 'l', 7),
+  ('Pasta', 0, 'kg', 12),
+  ('Bacon', 13, 'kg', 7),
+  ('Lemons', 20, 'kg', 9),
+  ('Olive Oil', 35, 'l', 20),
+  ('Paprika', 12, 'kg', 5),
+  ('Flour', 45, 'kg', 30),
+  ('Chocolate', 5, 'kg', 3),
+  ('Black Beans', 6, 'kg', 5)
