d7311b1abb8c4979d8b2c4ec42e4f921b9e246fd
MintMissy <dawidkostka480@gmail.com>
2021-12-06 18:27:45 +0100

    Write meals generator; Create meals insert for db

--- dataGenerators/records_generators.py
+++ dataGenerators/records_generators.py
@@ -1,7 +1,5 @@
+import random
 from random import randint
-min_storage_amount = 0
-max_storage_amount = 100
-
 
 def generate_client(amount):
     pass
@@ -11,26 +9,28 @@ def generate_employee(amount):
     pass
 
 
-def generate_meal():
-    pass
+def generate_meals(meal_names, min_price, max_price):
+    f = open("meals.txt", "w")
+
+    last_meal_name = meal_names[-1]
+    f.write("INSERT INTO `meals`(`name`, `cost_net`) VALUES\n")
+    for meal_name in meal_names:
+        meal_cost = random.uniform(min_price, max_price)
+        meal_cost = round(meal_cost, 2)
+
+        if meal_name != last_meal_name:
+            f.write(f"(\"{meal_name}\", {meal_cost}),\n")
+        else:
+            f.write(f"(\"{meal_name}\", {meal_cost})")
+
+    f.close()
+
+
 
 
 def generate_order():
     pass
 
-
-storage = {
-    "White Wine": "",
-    "Red Wine": "L",
-    "Milk": "L",
-    "Pasta": "Kg",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-}
 
 # Clients
 # ID NAME SURNAME SEX RESIDENCE POSTCODE PHONENUMBER
@@ -43,6 +43,3 @@ storage = {
 
 # Orders
 # ID CLIENT_ID MEAL_ID QUANTITY DELIVERY_PLACE DELIVERY_POSTCODE ORDER_DATE SHIP_DATE PICKUP_DATE
-
-# Storage
-# ID ITEM_NAME ITEM_QUANTITY QUANTITY_UNIT RECOMMENDED_QUANTITY
